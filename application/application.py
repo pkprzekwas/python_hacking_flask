9156812d65630df6e7158f1f282a642f8612b75b
Patryk Przekwas <patryk.przekwas@intel.com>
2017-08-02 16:26:39 +0000

    initial commit

---
+++ application/application.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, flash
+from forms import BookmarkForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = \
+    '\xb7\x06\xca\xbe\x8f\xda\xf9\x95A\tY+\x1as\xa7\xc7\x11;\xa9i\xc48\xb5o'
+
+bookmarks = []
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url=url,
+        user='Patryk',
+        date=datetime.utcnow()
+    ))
+
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', new_bookmarks=new_bookmarks(5))
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url)
+        flash('Stored bookmark: {}'.format(description))
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
