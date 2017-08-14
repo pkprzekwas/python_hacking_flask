1650ec751e8984bfc1a91f79e9f57e313794f22b
pkprzekwas <pkprzekwas@gmail.com>
2017-08-15 00:10:36 +0200

    models relations implemented

--- src/views.py
+++ src/views.py
@@ -2,7 +2,12 @@ from flask import render_template, redirect, url_for, flash
 
 from src import app, db
 from src.forms import BookmarkForm
-from src.models import Bookmark
+from src.models import Bookmark, User
+
+
+# Fake login
+def logged_in_user():
+    return User.query.filter_by(username='Patryk').first()
 
 
 @app.route('/')
@@ -17,12 +22,18 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        bm = Bookmark(url=url, description=description)
+        bm = Bookmark(user=logged_in_user(), url=url, description=description)
         db.session.add(bm)
         db.session.commit()
         flash('Stored bookmark: {}'.format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
+
+
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
 
 
 @app.errorhandler(404)
